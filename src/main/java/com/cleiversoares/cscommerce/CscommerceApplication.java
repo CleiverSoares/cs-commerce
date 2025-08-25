e8399fcb537644863366f8cea246b3c8ef8481c4
CleiverSoares <cleiversoares2@gmail.com>
2025-08-25 05:45:52 -0300

    feat: estrutura inicial do projeto

---
+++ src/main/java/com/cleiversoares/cscommerce/CscommerceApplication.java
@@ -0,0 +1,13 @@
+package com.cleiversoares.cscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CscommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CscommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleiversoares/cscommerce/CscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleiversoares.cscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
