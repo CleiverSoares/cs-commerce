ae8b924c8e74d10c2e97bac069b9640d20ab894d
CleiverSoares <cleiversoares2@gmail.com>
2025-08-28 07:07:25 -0300

    feat: adiciona método delete no ProductController e no ProductService para remoção de produtos

--- src/main/java/com/cleiversoares/cscommerce/controllers/ProductController.java
+++ src/main/java/com/cleiversoares/cscommerce/controllers/ProductController.java
@@ -45,5 +45,12 @@ public class ProductController {
     public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
         dto = service.update(id, dto);
         return ResponseEntity.ok(dto);
+
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
