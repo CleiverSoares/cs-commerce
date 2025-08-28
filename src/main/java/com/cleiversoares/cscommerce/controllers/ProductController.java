1bd6312cc1bd213f2241922dabf8a23b21e7b4ed
CleiverSoares <cleiversoares2@gmail.com>
2025-08-28 06:03:53 -0300

    feat: adiciona método findAll no ProductController e no ProductService para suporte à paginação de produtos

--- src/main/java/com/cleiversoares/cscommerce/controllers/ProductController.java
+++ src/main/java/com/cleiversoares/cscommerce/controllers/ProductController.java
@@ -1,16 +1,16 @@
 package com.cleiversoares.cscommerce.controllers;
 
-import com.cleiversoares.cscommerce.dto.ProductDTO;
-import com.cleiversoares.cscommerce.entities.Product;
-import com.cleiversoares.cscommerce.repositories.ProductRepository;
-import com.cleiversoares.cscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import com.cleiversoares.cscommerce.dto.ProductDTO;
+import com.cleiversoares.cscommerce.services.ProductService;
+
 
 @RestController
 @RequestMapping(value = "/products")
@@ -22,5 +22,11 @@ public class ProductController {
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id) {
         return service.findById(id);
+    }
+
+//    /products?size=12&page=0&sort=name,desc
+    @GetMapping()
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
     }
 }
