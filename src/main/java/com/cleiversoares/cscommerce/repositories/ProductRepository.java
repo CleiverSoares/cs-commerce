973100ac8fb0cfe43c2e15385e2e41aeadbf36c6
CleiverSoares <cleiversoares2@gmail.com>
2025-09-10 06:00:37 -0300

    feat: adiciona UserController e UserDTO para gerenciamento de usuários e recuperação de informações do usuário autenticado

--- src/main/java/com/cleiversoares/cscommerce/repositories/ProductRepository.java
+++ src/main/java/com/cleiversoares/cscommerce/repositories/ProductRepository.java
@@ -1,14 +1,18 @@
 package com.cleiversoares.cscommerce.repositories;
 
 import com.cleiversoares.cscommerce.entities.Product;
+import com.cleiversoares.cscommerce.entities.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query("SELECT p FROM Product p WHERE" +
             " UPPER(p.name) LIKE UPPER(CONCAT('%', :name, '%'))")
     Page<Product> searchByName(String name, Pageable pageable);
+
 }
